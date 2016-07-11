63c00f8f9ff5ab15a6bec3d9713048bb78bbf313
Fede Fernández <federico.f.b@47deg.com>
2016-07-11 09:05:05 +0200

    Includes the tests for arbitrary section

---
+++ src/test/scala/exercises/scalachecklib/ArbitrarySpec.scala
@@ -0,0 +1,42 @@
+package exercises.scalachecklib
+
+import org.scalacheck.Shapeless._
+import org.scalatest.Spec
+import org.scalatest.prop.Checkers
+import shapeless.HNil
+
+import scalachecklib.ArbitrarySection
+
+class ArbitrarySpec extends Spec with Checkers {
+
+  def `implicit arbitrary char` = {
+
+    check(
+      Test.testSuccess(
+        ArbitrarySection.implicitArbitraryChar _,
+        Seq('A', 'E', 'I', 'O', 'U') :: HNil
+      )
+    )
+  }
+
+  def `implicit arbitrary case class` = {
+
+    check(
+      Test.testSuccess(
+        ArbitrarySection.implicitArbitraryCaseClass _,
+        false :: HNil
+      )
+    )
+  }
+
+  def `arbitrary on gen` = {
+
+    check(
+      Test.testSuccess(
+        ArbitrarySection.useArbitraryOnGen _,
+        8 :: HNil
+      )
+    )
+  }
+
+}
