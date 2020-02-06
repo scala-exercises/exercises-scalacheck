eb50d71646d7a3a93ac3299bf16f9185c2528e79
Scala Steward <me@scala-steward.org>
2020-02-06 20:32:17 +0100

    Update scalacheck-shapeless_1.14 to 1.2.4

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -20,7 +20,7 @@ object ProjectPlugin extends AutoPlugin {
       val scalatest: String           = "3.1.0"
       val scalatestplusScheck: String = "3.1.0.0-RC2"
       val scalacheck: String          = "1.14.2"
-      val scalacheckShapeless: String = "1.2.3"
+      val scalacheckShapeless: String = "1.2.4"
       val scalacheckDatetime: String  = "0.3.2"
       val jodaTime: String            = "2.10.5"
     }
